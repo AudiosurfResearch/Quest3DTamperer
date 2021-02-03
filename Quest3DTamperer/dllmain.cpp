3f012f25938d6ebd81edc4a72720de86499f25fa
RubberDuckShobe <dog@dog.dog>
2021-02-03 14:12:20 +0100

    Add channel group loading functionality

--- Quest3DTamperer/dllmain.cpp
+++ Quest3DTamperer/dllmain.cpp
@@ -12,6 +12,7 @@
 
 //The Quest3D SDK isn't included in this repo!
 //You'll have to get it yourself.
+//Sorry.
 #include <A3d_List.h>
 #include <A3d_ChannelGroup.h>
 #include <A3d_EngineInterface.h>
@@ -29,6 +30,7 @@ static void(__thiscall* TrueCallChannel)(A3d_Channel* self) = nullptr;
 //because calling the functions like a normal human being results in something something ESP value not being saved properly
 //and then the game dies
 //maybe because my SDK version is too new but how am i supposed to get an old version of the SDK
+//it might be too old actually, what kind of parallel universe is this now?
 static const char* (__thiscall* ChannelGroup_GetChannelGroupFileName)(A3d_ChannelGroup* self) = nullptr;
 static const char* (__thiscall* ChannelGroup_GetPoolName)(A3d_ChannelGroup* self) = nullptr;
 static int (__thiscall* ChannelGroup_GetChannelCount)(A3d_ChannelGroup* self) = nullptr;
@@ -37,15 +39,18 @@ static bool(__thiscall* ChannelGroup_GetReadOnly)(A3d_ChannelGroup* self) = null
 static void(__thiscall* ChannelGroup_SetGroupIsProtected)(A3d_ChannelGroup* self, bool newValue) = nullptr;
 static void(__thiscall* ChannelGroup_SetReadOnly)(A3d_ChannelGroup* self, bool newValue) = nullptr;
 static bool(__thiscall* ChannelGroup_SaveChannelGroup)(A3d_ChannelGroup* self, const char* fileName) = nullptr;
+static int(__thiscall* ChannelGroup_GetGroupIndex)(A3d_ChannelGroup* self) = nullptr;
+static void(__thiscall* ChannelGroup_CallStartChannel)(A3d_ChannelGroup* self) = nullptr;
 
 static bool init = false;
 bool showMenu;
 HWND gameHandle;
 WNDPROC g_WndProc_o;
-ImGui::FileBrowser fileDialog(ImGuiFileBrowserFlags_EnterNewFilename | ImGuiFileBrowserFlags_CreateNewDir);
-
+ImGui::FileBrowser saveGroupFileDialog(ImGuiFileBrowserFlags_EnterNewFilename | ImGuiFileBrowserFlags_CreateNewDir);
+ImGui::FileBrowser loadGroupFileDialog(0);
 EngineInterface* engine = nullptr;
 int channelGroupToUse = 0;
+char newGroupName[128] = "New Group";
 
 static void __fastcall CallChannelHook(A3d_Channel* self, DWORD edx)
 {
@@ -84,8 +89,11 @@ long __stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice)
         ImGui_ImplWin32_Init(params.hFocusWindow);
         ImGui_ImplDX9_Init(pDevice);
 
-        fileDialog.SetTitle("Save channel group");
-        fileDialog.SetTypeFilters({ ".cgr" });
+        saveGroupFileDialog.SetTitle("Save channel group");
+        saveGroupFileDialog.SetTypeFilters({ ".cgr" });
+
+        saveGroupFileDialog.SetTitle("Load channel group");
+        saveGroupFileDialog.SetTypeFilters({ ".cgr" });
 
         init = true;
     }
@@ -95,45 +103,70 @@ long __stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice)
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
 
-        //Because audiosurf has its own cursor and it'll just be below the ImGUI window
+        //Because Audiosurf has its own cursor and it'll just be below the ImGUI window
         ImGui::GetIO().MouseDrawCursor = true;
 
         A3d_ChannelGroup* group = nullptr;
         
         ImGui::Begin("Quest3DTamperer");
-        ImGui::Text("yeah sex is cool but have you ever used Dear ImGUI");
+        // :^)
+        //ImGui::Text("yeah sex is cool but have you ever used Dear ImGUI");
         ImGui::Spacing();
-        if (ImGui::CollapsingHeader("Channel groups")) {
-            ImGui::Text("There is %i channel groups.", engine->GetChannelGroupCount());
-            ImGui::InputInt("A3d_ChannelGroup to use", &channelGroupToUse, 1, 10);
-            ImGui::Spacing();
-            group = engine->GetChannelGroup(channelGroupToUse);
-            if (group != nullptr) {
-                ImGui::Text("Info of current channel group:");
-                ImGui::Text(ChannelGroup_GetPoolName(group));
-                ImGui::Text(ChannelGroup_GetChannelGroupFileName(group));
-                ImGui::Text("Is group protected: %s", ChannelGroup_GetGroupIsProtected ? "true" : "false");
-                ImGui::Text("Is group read-only: %s", ChannelGroup_GetGroupIsProtected ? "true" : "false");
-                //returns a stupidly large number, most likely bugged?
-                //ImGui::Text("Group has %i channels", ChannelGroup_GetChannelCount(group));
-                if (ImGui::Button("Save without protection")) {
-                    ChannelGroup_SetReadOnly(group, false);
-                    ChannelGroup_SetGroupIsProtected(group, false);
-                    fileDialog.Open();
+        if (engine != nullptr) {
+            if (ImGui::CollapsingHeader("Channel groups")) {
+                ImGui::Text("There is %i channel groups.", engine->GetChannelGroupCount());
+                ImGui::Spacing();
+                ImGui::InputText("Pool name for new group", newGroupName, IM_ARRAYSIZE(newGroupName));
+                if (ImGui::Button("Load channel group file")) {
+                    loadGroupFileDialog.Open();
+                }
+                ImGui::Spacing();
+                ImGui::InputInt("A3d_ChannelGroup to use", &channelGroupToUse, 1, 10);
+                ImGui::Spacing();
+                group = engine->GetChannelGroup(channelGroupToUse);
+                if (group != nullptr) {
+                    ImGui::Text("Info of current channel group:");
+                    ImGui::Text(ChannelGroup_GetPoolName(group));
+                    ImGui::Text(ChannelGroup_GetChannelGroupFileName(group));
+                    ImGui::Text("Is group protected: %s", ChannelGroup_GetGroupIsProtected ? "true" : "false");
+                    ImGui::Text("Is group read-only: %s", ChannelGroup_GetGroupIsProtected ? "true" : "false");
+                    //returns a stupidly large number, most likely bugged?
+                    //ImGui::Text("Group has %i channels", ChannelGroup_GetChannelCount(group));
+                    if (ImGui::Button("Save group without protection")) {
+                        ChannelGroup_SetReadOnly(group, false);
+                        ChannelGroup_SetGroupIsProtected(group, false);
+                        saveGroupFileDialog.Open();
+                    }
+                }
+                else {
+                    ImGui::Text("Group not found!");
                 }
             }
-            else {
-                ImGui::Text("Group not found!");
-            }
+        }
+        else {
+            ImGui::Text("Please perform any action in-game.\nThis is needed to get the pointer to the EngineInterface,\nwhich is neccessary for Quest3DTamperer to work.");
         }
         ImGui::End();
 
-        fileDialog.Display();
+        saveGroupFileDialog.Display();
+        loadGroupFileDialog.Display();
 
-        if (fileDialog.HasSelected())
+        if (saveGroupFileDialog.HasSelected())
         {
-            ChannelGroup_SaveChannelGroup(group, fileDialog.GetSelected().string().c_str());
-            fileDialog.ClearSelected();
+            ChannelGroup_SaveChannelGroup(group, saveGroupFileDialog.GetSelected().string().c_str());
+            saveGroupFileDialog.ClearSelected();
+        }
+        if (loadGroupFileDialog.HasSelected())
+        {
+            A3d_ChannelGroup* newGroup = engine->LoadChannelGroup(loadGroupFileDialog.GetSelected().string().c_str(), newGroupName);
+            //Call the group's start channel
+            if (newGroup != nullptr)
+            {
+                ChannelGroup_CallStartChannel(newGroup);
+            }
+            //literally crashes the game
+            //channelGroupToUse = ChannelGroup_GetGroupIndex(newGroup);
+            loadGroupFileDialog.ClearSelected();
         }
 
         ImGui::EndFrame();
@@ -202,6 +235,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
         CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)kieroExampleThread, NULL, 0, NULL);
 
         DetourRestoreAfterWith();
+        //welcome to HELL
         TrueCallChannel =
             (void(__thiscall*)(A3d_Channel*))
             DetourFindFunction("highpoly.dll", "?CallChannel@A3d_Channel@@UAEXXZ");
@@ -229,6 +263,12 @@ BOOL APIENTRY DllMain( HMODULE hModule,
         ChannelGroup_SaveChannelGroup =
             (bool(__thiscall*)(A3d_ChannelGroup*, const char*))
             DetourFindFunction("highpoly.dll", "?SaveChannelGroup@A3d_ChannelGroup@@UAE_NPBD@Z");
+        ChannelGroup_GetGroupIndex =
+            (int(__thiscall*)(A3d_ChannelGroup*))
+            DetourFindFunction("highpoly.dll", "?GetGroupIndex@A3d_ChannelGroup@@UAEHXZ");
+        ChannelGroup_CallStartChannel =
+            (void(__thiscall*)(A3d_ChannelGroup*))
+            DetourFindFunction("highpoly.dll", "?CallStartChannel@A3d_ChannelGroup@@UAEXXZ");
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
         DetourAttach(&(PVOID&)TrueCallChannel, CallChannelHook);
